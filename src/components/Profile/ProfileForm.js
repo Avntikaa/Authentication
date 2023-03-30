e5a4c02ade6a42fa11d6217326179bfbd0e9fc67
Avantika <99116667+Avntikaa@users.noreply.github.com>
2023-03-30 22:41:46 +0530

    update old password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,14 +1,52 @@
+import { useRef } from 'react';
+import { useStateContext } from '../../store/StateContext';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+const cxt=useStateContext();
+console.log(cxt.token);
+  const newp=useRef();
+  async function changePassword(e){
+    e.preventDefault();
+const newPassword=newp.current.value;
+try{
+const res=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD9GUyT_XYB3Ud1rD-7P0hYccPO8U_v6tw',{
+  method:'POST',
+  body:JSON.stringify({
+    idToken:cxt.token,
+password:newPassword,
+returnSecureToken:true
+  }),
+  headers:{
+    'Content-Type':'application/json'
+  }
+})
+if(res.ok){
+  res.json().then((data)=>{
+console.log(data.idToken);
+  });
+}
+else{
+  console.log(res.json());
+}
+    // cxt.setIsLogin((prevState) => !prevState);
+// cxt.setLoginPage(false);
+}
+catch(error){
+  //  setIsAlert(true);
+  //  setIsLoader(false);
+}
+}
+
+
   return (
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newp}/>
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button onClick={changePassword}>Change Password</button>
       </div>
     </form>
   );
